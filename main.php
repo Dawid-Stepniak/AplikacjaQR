b292e2d6c7de4e4b742c7ab24aa1c2f62dcfd49b
Dawid-Stepniak <dawid.stepniak2006@gmail.com>
2024-05-15 14:12:36 +0200

    Add library directory with TCPDF class in it.

--- main.php
+++ main.php
@@ -1,1 +1,27 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Generator kodów QR</title>
+</head>
+<body>
+    <h1>Wygeneruj swój kod QR</h1>
+    <br>
+    <form>
+        <label for="posX">Pozycja X</label>
+        <input type="number" name="posX">
+        <br><br>
+        <label for="posY">Pozycja Y</label>
+        <input type="number" name="posX">
+        <br><br>
+        <label for="imgSelect">Wybierz tło</label>
+        <input type="file" name="imgSelect" accept=".png,.jpg,.jpeg">
+        <br><br>
+        <label for="csvSelect">Wybierz plik csv</label>
+        <input type="file" name="csvSelect" accept=".csv">
+        <br><br>
+        <input type="submit">
+    </form>
+</body>
+</html>
